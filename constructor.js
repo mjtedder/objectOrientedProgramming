501a604ae299a6d4ad1732fbc47bcc2ae4adf00e
mjtedder <mjtedder@gmail.com>
2019-04-02 17:12:34 -0600

    Add static method to Vehicle class
    I wanted to demonstrate that static methods do not apply to objects,
    only the constructor on which it is bound.  In this case, the Vehicle
    class.

--- constructor.js
+++ constructor.js
@@ -24,3 +24,32 @@ console.log(prius.year);
 
 // Prints "3" to the console
 console.log(mazda.name);
+
+//ES6 Class constructor================================================
+//=========================================================================================
+class Vehicle {
+    constructor(name, year, make, mileage) {
+        this.name = name;
+        this.year = year;
+        this.make = make;
+        this.mileage = 3000;
+    }
+    roadTrip() {
+        this.mileage += 400;
+    }
+    static staticMethod() {
+        console.log("I only am bound to the Vehicle class, not an object.");
+    }
+}
+
+const volvo = new Vehicle ("V60", 2018 ,"Volvo")
+
+// This method increases mileage by 400.
+volvo.roadTrip();
+console.log(volvo.mileage);
+
+console.log("Prints undefined, because it's an object not a class: " + volvo.staticMethod);
+
+Vehicle.staticMethod();
+
+//
